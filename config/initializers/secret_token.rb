2847806f1772874b9dfd8699e96fa94b98b25832
David La Cross <david.lacross@gmail.com>
2012-02-04 23:59:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmbedTester::Application.config.secret_token = 'db223a845534ee1d1dc2c354475965972e1aa1e182dd606d3006840b8d1fe7aac74611e0eef4922e375af2273c7392fd3445132328d48e90904e64841a960d22'
